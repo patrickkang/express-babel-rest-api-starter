c7247623257cb8ad21cc3dd9045dc0182092191e
Patrick Kang <jykang1@gmail.com>
2018-02-14 11:04:51 -0500

    adds node-convict

--- src/config.js
+++ src/config.js
@@ -1,8 +1,21 @@
 import 'dotenv/config';
+import convict from 'convict';
 
-const config = {
-  env: process.env.NODE_ENV,
-  port: process.env.PORT,
-};
+const config = convict({
+  env: {
+    doc: 'The application environment',
+    format: ['development', 'production', 'test'],
+    default: 'development',
+    env: 'NODE_ENV',
+  },
+  port: {
+    doc: 'The port to bind',
+    format: "port",
+    default: 3000,
+    env: "PORT",
+  },
+});
 
-export default config;
+config.validate({allowed: 'strict'});
+
+export default config.getProperties();
